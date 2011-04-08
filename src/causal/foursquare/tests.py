4410f52b14dd4e8ade6516042f841d7d31a39632
Wesley Mason <wes@1stvamp.org>
2011-04-08 07:34:08 +0100

    Refactored json loading in 4sq test

--- src/causal/foursquare/tests.py
+++ src/causal/foursquare/tests.py
@@ -17,25 +17,26 @@ except ImportError:
 class TestFoursquareViews(TestCase):
     """Test the module with fixtures.
     """
-    
+
     def setUp(self):
         self.path = os.path.dirname(os.path.realpath(__file__))
         self.handler = ServiceHandler(model_instance=UserService())
-        
+
     def tearDown(self):
         pass
+
+    def _loadJson(self, name):
+        return simplejson.load(file(os.path.join(self.path, 'test_data', name)))
 
     def test_convert_feed(self):
         """Check we do the right thing for converting
         foursquares feed into ours."""
-        
-        json_file = open(os.path.join(self.path, 'test_data', 'user_history.json'), 'r')
-        json_feed = json_file.read()
-        json_file.close()
-        
-        since = datetime.strptime('Tue, 01 Feb 11 06:40:17 +0000'.replace(' +0000', ''), 
+
+        json = self._loadJson('user_history.json')
+
+        since = datetime.strptime('Tue, 01 Feb 11 06:40:17 +0000'.replace(' +0000', ''),
                           '%a, %d %b %y %H:%M:%S')
-        
-        results = self.handler._convert_feed(simplejson.loads(json_feed), since.date())
-        
+
+        results = self.handler._convert_feed(json, since.date())
+
         self.assertEqual(len(results), 3)
