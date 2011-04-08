fea54cb3946c745975eab7ccca24303d106e63d1
Wesley Mason <wes@1stvamp.org>
2011-04-08 07:41:45 +0100

    Fixed some bad markup in twitpic/yfrog formatters

--- src/causal/main/tests.py
+++ src/causal/main/tests.py
@@ -1,9 +1,8 @@
 from django.test import TestCase
-from causal.main.models import User, OAuthSetting
-from causal.main.views import history
+from causal.main.models import User
+from causal.main.views import history, _add_image_html
 from django.test.client import Client
 from django.core.urlresolvers import reverse
-from views import _add_image_html
 
 try:
     import wingdbstub
@@ -11,9 +10,9 @@ except ImportError:
     pass
 
 class TestMain(TestCase):
-    """Test the module with fixtures."""
+    """Test the module with fixtures.
+    """
 
-    #fixtures = ['oauth_settings.json']
     user_details = {
         'username': 'test_user',
         'email': 'test_user@example.com',
@@ -21,7 +20,9 @@ class TestMain(TestCase):
     }
 
     def setUp(self):
-        """Make sure we have a user and any other bits and bots for tests"""
+        """Make sure we have a user and any other bits and bots for tests
+        """
+
         self.user = User.objects.create_user(**self.user_details)
         self.user.save()
 
@@ -29,14 +30,17 @@ class TestMain(TestCase):
         self.user.delete()
 
     def test_add_image_html_twitpic(self):
-        """Test replacing twitpic text with a link to twitpic."""
+        """Test replacing twitpic text with a link to twitpic.
+        """
+
         tweet = "Some text #ahashtag http://twitpic.com/354bkr some more text."
-        converted_tweet = 'Some text #ahashtag http://twitpic.com/354bkr some more text. </br> <a href="http://twitpic.com/354bkr"><img src="http://twitpic.com/show/mini/354bkr"/></a>'
+        converted_tweet = 'Some text #ahashtag http://twitpic.com/354bkr some more text. <br/> <a href="http://twitpic.com/354bkr"><img src="http://twitpic.com/show/mini/354bkr"/></a>'
         self.assertEqual(_add_image_html(tweet), converted_tweet)
-        
+
     def test_add_image_html_yfrogpic(self):
-        """Test replacing yfrog pic text with a link to yfrog pic."""
+        """Test replacing yfrog pic text with a link to yfrog pic.
+        """
+
         tweet = "Some text #ahashtag http://yfrog.com/b9hmwkj some more text."
-        converted_tweet = 'Some text #ahashtag http://yfrog.com/b9hmwkj some more text. </br> <a href="http://yfrog.com/b9hmwkj"><img src="http://yfrog.com/b9hmwkj.th.jpg"/></a>'
+        converted_tweet = 'Some text #ahashtag http://yfrog.com/b9hmwkj some more text. <br/> <a href="http://yfrog.com/b9hmwkj"><img src="http://yfrog.com/b9hmwkj.th.jpg"/></a>'
         self.assertEqual(_add_image_html(tweet), converted_tweet)
-        
